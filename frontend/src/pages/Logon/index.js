3a69485ab17a09839ef3ea180403cd4eec255160
Gabriel Arruda <gabrielaugusto.arruda@gmail.com>
2020-04-04 23:24:28 -0300

    adicionando Register na landing page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import { Link, useHistory } from 'react-router-dom'
 
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiArrowLeft } from 'react-icons/fi'
 
 import api from '../../services/api';
 
@@ -12,6 +12,7 @@ import logoImg from '../../assets/logo.svg';
 import logoImgNoNameW from '../../assets/logo_noname_w.svg';
 import logoImgNoNameG from '../../assets/logo_noname_g.svg'
 import heroesImg from '../../assets/Logon/parthenon_logo.png';
+
 
 export default function Logon() {
     const [id, setId] = useState('');
@@ -35,12 +36,43 @@ export default function Logon() {
         }
     }
 
+    {/* ----------------REGISTER---------------*/ }
+    const [name, setName] = useState('');
+    const [institution, setInstitution] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            name,
+            institution,
+            email,
+            phone,
+            city,
+            uf,
+        };
+
+        try {
+            const response = await api.post('ongs', data)
+
+            alert(`Seu ID de acesso: ${response.data.id}`)
+            history.push('/');
+        } catch (err) {
+            alert(`Erro ao Realizar Cadastro, tente novamente`)
+        }
+    }
+    {/*---------------------------------------------- */ }
+
     return (
         <div className="logon-container">
             <header>
                 <img src={logoImgNoNameW}
-                    onMouseOver={e=>(e.currentTarget.src =logoImgNoNameG)}
-                    onMouseOut={e=>(e.currentTarget.src = logoImgNoNameW)}
+                    onMouseOver={e => (e.currentTarget.src = logoImgNoNameG)}
+                    onMouseOut={e => (e.currentTarget.src = logoImgNoNameW)}
                     alt="Parthenon Community"
                 />
                 <nav className="buttonNav">
@@ -71,6 +103,66 @@ export default function Logon() {
                     </form>
                 </section>
             </div>
+
+
+
+
+            {/*--------------------------REGISTER---------------------------------------*/}
+
+
+
+
+
+            <section className="register-section" >
+                <div className="register-container">
+                    <div className="content">
+                        <section>
+                            <h1>Venha fazer parte da nossa comunidade científica</h1>
+                            <p>Como um usuário premium, você poderá baixar licenças que permitem usar todos os recursos incluídos no Freepik Selection sem atribuição. Além disso, seu limite de downloads será alterado de 10 para 100 por dia. Como usuário Premium, você também terá acesso ao nosso novo conteúdo exclusivo para assinantes.</p>
+                            <p>Como Usuário Premium, você também terá acesso ao nosso novo Conteúdo Premium. Esse estoque de imagens contém designs exclusivos que só poderão ser baixados por Usuários Premium.</p>
+
+                            <Link className="back-link" to="/">
+                                <FiArrowLeft size={16} color="#FFF" />
+                        Já tenho cadastro
+                    </Link>
+                        </section>
+
+                        <form className ="formRegister" onSubmit={handleRegister}>
+
+                            <input placeholder="Nome completo"
+                                value={name}
+                                onChange={e => setName(e.target.value)} />
+
+                            <input type="institution" placeholder="Instituição"
+                                value={institution}
+                                onChange={e => setInstitution(e.target.value)} />
+
+                            <input type="email" placeholder="E-mail"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)} />
+
+                            <input placeholder="Telefone"
+                                value={phone}
+                                onChange={e => setPhone(e.target.value)} />
+
+                            <div className="input-group">
+                                <input placeholder="Cidade"
+                                    value={city}
+                                    onChange={e => setCity(e.target.value)} />
+
+                                <input placeholder="UF" style={{ width: 80 }}
+                                    value={uf}
+                                    onChange={e => setUf(e.target.value)} />
+                            </div>
+
+                            <button className="button" type="submit">Cadastrar</button>
+
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+
             <footer>
                 <p>2020 © Tekmor</p>
             </footer>
